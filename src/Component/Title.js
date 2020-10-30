f9784419ca1a58af4fab4e34600079211038a0cb
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-30 11:03:54 +0530

    Title

---
+++ src/Component/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Weather Finder</h1>
+                <p> Find Out temperature conditions and more.</p>
+            </div>
+        )
+    }
+}
+export default Title;
