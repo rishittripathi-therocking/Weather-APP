6e124012efea1628babd1a7dd99eaf2334a25ec8
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-30 11:36:23 +0530

    Dynamic weather

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import Weather from './Component/Weather';
 const API_KEY = "5fd9750c30cc3cac0559b3807c18a8be";
 
 class App extends React.Component {
-    getWeather = async() => {
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`); 
+    getWeather = async(e) => {
+        e.preventDefault();
+        const city=e.target.elements.city.value;
+        const country=e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`); 
         const data = await api_call.json();
         console.log(data);
 
